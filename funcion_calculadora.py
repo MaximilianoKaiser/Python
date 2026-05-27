b5b6dc6722763982d2431e447108c86d98183752
MaximilianoKaiser <Maniotti.Kaiser@gmail.com>
2026-05-27 20:33:10 -0300

    Create funcion_calculadora.py
    Esta calculadora está para ayudarlos a entender funciones.

---
+++ funcion_calculadora.py
@@ -0,0 +1,13 @@
+def calculadora(num1, num2):
+    suma = num1 + num2
+    resta = num1 - num2
+    Divition = num1 / num2
+    Multi = num1 * num2
+
+    print(f"esta es la suma: {suma}")
+    print (f"esta es la resta :{resta}")
+    print (f" esta es la divición {Divition}")
+    print (f" esta es la multiplicación {Multi}")
+
+
+calculadora (8, 2)
